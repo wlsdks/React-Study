7b276adaf504fa64e5ff203f6119e2d887c86242
jinan <ja.choi@sweetk.co.kr>
2023-04-11 16:18:51 +0900

    1. 잘못된 경로 페이지에서 back버튼 누르고 뒤로 돌아가기 기능 추가

--- react-standard/src/components/NotFoundPage.tsx
+++ react-standard/src/components/NotFoundPage.tsx
@@ -1,10 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+    height: 30px;
+    width: 60px;
+`;
+
 /**
  * @description 잘못된 경로로 들어왔을때 보여주는 컴포넌트
  */
 function NotFoundPage() {
+    // router-info
+    const navigate = useNavigate();
+
+    // event
+    const onBack = () => {
+        navigate(-1);
+    };
+
+    // view
     return (
         <>
             <h1>잘못된 경로로 접근하였습니다.</h1>
+            <Button onClick={onBack}>Back</Button>
         </>
     );
 }
