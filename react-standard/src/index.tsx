6ab559b1eb4083f06e9c9d54b30d62665880d567
jinan <ja.choi@sweetk.co.kr>
2023-04-11 16:11:24 +0900

    1. 라우터 기능 추가
    1. -yarn add react-router
    2. index.tsx 수정
    3. router.js 추가

--- react-standard/src/index.tsx
+++ react-standard/src/index.tsx
@@ -1,12 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/App";
+import router from "@/router";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
---
+++ react-standard/src/router.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>ROUTER</h1>,
+    },
+]);
+
+export default router;
