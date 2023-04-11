a8b95208aab106af01b54ba9d2edfd3d3bc2fe24
jinan <ja.choi@sweetk.co.kr>
2023-04-11 16:15:55 +0900

    1. 404페이지로 이동시켜주는 코드 추가하기
    1. errorElement 추가

--- react-standard/src/router.js
+++ react-standard/src/router.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "@/App";
+import NotFoundPage from "@/components/NotFoundPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <NotFoundPage />,
     },
 ]);
 
